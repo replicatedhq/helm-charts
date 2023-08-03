c837d140d46fb70413bf76fcd91fb78cb868a8c4
Diamon Wiggins <diamonw757@gmail.com>
2023-08-03 17:46:45 -0400

    restructure tests to use a struct for test cases and the mock k8s client

---
+++ test/deployment_test.go
@@ -0,0 +1,88 @@
+package test
+
+import (
+	"context"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/gruntwork-io/terratest/modules/helm"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	testclient "k8s.io/client-go/kubernetes/fake"
+)
+
+func TestDeployment_ExpectedContainers(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name               string
+		values             map[string]string
+		expectedContainers int
+	}{
+		{
+			name: "single app with single container",
+			values: map[string]string{
+				"apps.example.enabled":                             "true",
+				"apps.example.type":                                "deployment",
+				"apps.example.containers.example.image.repository": "nginx",
+				"apps.example.containers.example.image.tag":        "latest",
+			},
+			expectedContainers: 1,
+		},
+		{
+			name: "single app with multiple containers",
+			values: map[string]string{
+				"apps.example.enabled":                              "true",
+				"apps.example.type":                                 "deployment",
+				"apps.example.containers.example.image.repository":  "nginx",
+				"apps.example.containers.example.image.tag":         "latest",
+				"apps.example.containers.example2.image.repository": "nginx",
+				"apps.example.containers.example2.image.tag":        "9.9.9",
+			},
+			expectedContainers: 2,
+		},
+		{
+			name: "multiple apps with a single container",
+			values: map[string]string{
+				"apps.example.enabled":                              "true",
+				"apps.example.type":                                 "deployment",
+				"apps.example.containers.example.image.repository":  "nginx",
+				"apps.example.containers.example.image.tag":         "latest",
+				"apps.example2.enabled":                             "true",
+				"apps.example2.type":                                "deployment",
+				"apps.example2.containers.example.image.repository": "nginx",
+				"apps.example2.containers.example.image.tag":        "9.9.9",
+			},
+			expectedContainers: 2,
+		},
+	}
+
+	helmChartPath, err := filepath.Abs("../charts/test")
+	releaseName := "release-name"
+
+	for _, tt := range tests {
+		options := &helm.Options{
+			SetValues:         tt.values,
+			BuildDependencies: true,
+		}
+
+		output := helm.RenderTemplate(t, options, helmChartPath, releaseName, []string{"templates/replicated-library.yaml"})
+
+		ctx := context.Background()
+		client := testclient.NewSimpleClientset()
+
+		err = k8sApply(ctx, client, []byte(output))
+		require.NoError(t, err)
+
+		deployments, err := client.AppsV1().Deployments("default").List(ctx, metav1.ListOptions{})
+		require.NoError(t, err)
+
+		totalContainers := 0
+
+		for _, deployment := range deployments.Items {
+			totalContainers = totalContainers + len(deployment.Spec.Template.Spec.Containers)
+		}
+		require.Equal(t, tt.expectedContainers, totalContainers)
+	}
+}
---
+++ test/util.go
@@ -0,0 +1,45 @@
+package test
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+
+	appsv1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/kubectl/pkg/scheme"
+)
+
+func k8sDecode(data []byte) (runtime.Object, error) {
+	decode := scheme.Codecs.UniversalDeserializer().Decode
+	obj, _, err := decode(data, nil, nil)
+	return obj, err
+}
+
+func k8sApply(ctx context.Context, client kubernetes.Interface, yaml []byte) error {
+	yamlFiles := bytes.Split(yaml, []byte("---"))
+	for _, f := range yamlFiles {
+		if len(f) == 0 || string(f) == "\n" {
+			continue
+		}
+
+		obj, err := k8sDecode(f)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		namespace := "default"
+
+		switch o := obj.(type) {
+		case *appsv1.Deployment:
+			_, err := client.AppsV1().Deployments(namespace).Create(ctx, o, metav1.CreateOptions{})
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
