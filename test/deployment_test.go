66a2d66e2feeb31d3eb0f76777c9786f9df2212d
Diamon Wiggins <diamonw757@gmail.com>
2023-08-06 19:53:24 -0400

    trying monolith test function

--- test/deployment_test.go
+++ test/deployment_test.go
@@ -13,7 +13,7 @@ import (
 )
 
 func TestDeployment_ExpectedNumOfContainers(t *testing.T) {
-	//t.Parallel()
+	t.Parallel()
 
 	tests := []struct {
 		name               string
@@ -87,57 +87,50 @@ func TestDeployment_ExpectedNumOfContainers(t *testing.T) {
 	}
 }
 
-func TestDeployment_ExpectedImages(t *testing.T) {
-	//t.Parallel()
-
-	tests := []struct {
-		name          string
-		values        map[string]string
-		expectedImage string
-	}{
-		{
-			name: "nginx latest",
-			values: map[string]string{
-				"apps.example.enabled":                             "true",
-				"apps.example.type":                                "deployment",
-				"apps.example.containers.example.image.repository": "nginx",
-				"apps.example.containers.example.image.tag":        "latest",
-			},
-			expectedImage: "nginx:latest",
-		},
-		{
-			name: "nginx 9.9.9",
-			values: map[string]string{
-				"apps.example.enabled":                             "true",
-				"apps.example.type":                                "deployment",
-				"apps.example.containers.example.image.repository": "nginx",
-				"apps.example.containers.example.image.tag":        "9.9.9",
-			},
-			expectedImage: "nginx:9.9.9",
-		},
-	}
+func TestDeployment(t *testing.T) {
+	t.Parallel()
 
 	helmChartPath, err := filepath.Abs("../charts/test")
 	releaseName := "release-name"
 
-	for _, tt := range tests {
-		options := &helm.Options{
-			SetValues:         tt.values,
-			BuildDependencies: true,
-		}
-
-		output := helm.RenderTemplate(t, options, helmChartPath, releaseName, []string{"templates/replicated-library.yaml"})
-
-		ctx := context.Background()
-		client := testclient.NewSimpleClientset()
-
-		err = k8sApply(ctx, client, []byte(output))
-		require.NoError(t, err)
-
-		deployment, err := client.AppsV1().Deployments("default").Get(ctx, "release-name-test-example", metav1.GetOptions{})
-		require.NoError(t, err)
-
-		actualImage := deployment.Spec.Template.Spec.Containers[0].Image
-		require.Equal(t, tt.expectedImage, actualImage)
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"apps.example.enabled":                              "true",
+			"apps.example.type":                                 "deployment",
+			"apps.example.containers.example.image.repository":  "nginx",
+			"apps.example.containers.example.image.tag":         "latest",
+			"apps.example.containers.example2.image.repository": "nginx",
+			"apps.example.containers.example2.image.tag":        "9.9.9",
+		},
+		BuildDependencies: true,
 	}
+
+	output := helm.RenderTemplate(t, options, helmChartPath, releaseName, []string{"templates/replicated-library.yaml"})
+
+	ctx := context.Background()
+	client := testclient.NewSimpleClientset()
+
+	err = k8sApply(ctx, client, []byte(output))
+	require.NoError(t, err)
+
+	allDeployments, err := client.AppsV1().Deployments("default").List(ctx, metav1.ListOptions{})
+	require.NoError(t, err)
+
+	// Test Number of Containers in across all Deployments
+	totalContainers := 0
+
+	for _, deployment := range allDeployments.Items {
+		totalContainers = totalContainers + len(deployment.Spec.Template.Spec.Containers)
+	}
+	require.Equal(t, 2, totalContainers)
+
+	// Test Container Images
+	deployment, err := client.AppsV1().Deployments("default").Get(ctx, "release-name-test-example", metav1.GetOptions{})
+	require.NoError(t, err)
+
+	require.Equal(t, "nginx:latest", deployment.Spec.Template.Spec.Containers[0].Image)
+
+	require.Equal(t, "nginx:9.9.9", deployment.Spec.Template.Spec.Containers[1].Image)
+
+	// Test Something Else?
 }
